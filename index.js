4c87ced6b06439408a043592a99d174ff3c6472c
moizansari2024 <moizansari672@gmail.com>
2024-12-26 11:16:45 -0800

    js

---
+++ index.js
@@ -0,0 +1,23 @@
+var num = prompt("Enter your obtained marks");
+var num1 = prompt("Enter your total marks");
+var newnum = num * 100;
+var total = newnum / num1;
+var round = Math.round(total);
+alert(round + "%");
+
+// var city = prompt("Enter your city");
+// if(city === "Karchi"){
+// console.log("city is find");
+// } else(console.log("city is not find"));
+
+// var num = +prompt("Enter your number");
+// var newnum = +prompt("Enter your second number");
+// var total = num + newnum;
+// alert(total);
+
+
+
+
+// var user = prompt("Enter your name");
+// var userone = prompt("Enter your last name")
+// alert("Thanks" + " " + user + userone );
